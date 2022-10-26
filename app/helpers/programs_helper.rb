76099a73ebf83babcf78856d0700627efa305a30
Robert Cigan <cigan.robert@gmail.com>
2022-10-26 21:46:20 +0200

    time_between fix

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -1,5 +1,5 @@
 module ProgramsHelper
   def time_between(start_time, end_time) # "4:00 format"
-    (Time.zone.parse(start_time)..Time.zone.parse(end_time)).include?(Time.current)
+    (Time.zone.parse(start_time) <= Time.current) && (Time.zone.parse(end_time) >= Time.current)
   end
 end
