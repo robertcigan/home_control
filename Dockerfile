bd6789f5e4c553d26b252c166bf6c057489e14d3
Robert Cigan <cigan.robert@gmail.com>
2025-07-14 18:54:17 +0200

    fix downloading backup on docker setup with dedicated DB instance (#24)

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,9 @@ FROM ruby:3.3.6 AS home_control
 # Install yarn
 RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg -o /root/yarn-pubkey.gpg && apt-key add /root/yarn-pubkey.gpg
 RUN echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
-RUN apt-get update && apt-get install -y --no-install-recommends nodejs yarn build-essential libpq-dev zlib1g-dev liblzma-dev
+RUN apt-get update && apt-get install -y --no-install-recommends \
+  nodejs yarn build-essential libpq-dev zlib1g-dev liblzma-dev \
+  postgresql-client
 
 # Default directory
 ENV INSTALL_PATH="/app/home_control"
--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -5,7 +5,13 @@ class BackupsController < ApplicationController
   end
 
   def download
-    db_name = ActiveRecord::Base.connection.current_database
+    db_config = ActiveRecord::Base.connection_db_config
+    db_name = db_config.database
+    db_user = db_config.configuration_hash[:username]
+    db_host = db_config.configuration_hash[:host] || 'localhost'
+    db_port = db_config.configuration_hash[:port] || 5432
+    db_password = db_config.configuration_hash[:password]
+
     timestamp = Time.current.strftime("%Y%m%d_%H%M%S")
     format = params[:format]
 
@@ -13,19 +19,37 @@ class BackupsController < ApplicationController
     when 'sql'
       filename = "#{db_name}_#{timestamp}.sql"
       content_type = 'application/sql'
-      dump_cmd = "pg_dump --no-acl --no-owner #{db_name}"
+      format_flag = ''
     when 'custom'
       filename = "#{db_name}_#{timestamp}.dump"
       content_type = 'application/octet-stream'
-      dump_cmd = "pg_dump -Fc --no-acl --no-owner #{db_name}"
+      format_flag = '-Fc'
     else
       return head :bad_request
     end
 
+    dump_cmd = [
+      "PGPASSWORD='#{db_password}'",
+      "pg_dump",
+      format_flag,
+      "--no-acl --no-owner",
+      "-h #{db_host}",
+      "-p #{db_port}",
+      "-U #{db_user}",
+      db_name
+    ].join(' ')
+
     begin
       dump = `#{dump_cmd}`
-      send_data dump, filename: filename, type: content_type, disposition: 'attachment'
+      if $?.success?
+        send_data dump, filename: filename, type: content_type, disposition: 'attachment'
+      else
+        Rails.logger.error "pg_dump failed: #{dump}"
+        flash[:alert] = "Error creating backup."
+        redirect_to backup_path
+      end
     rescue => e
+      Rails.logger.error "Backup failed: #{e.message}"
       flash[:alert] = "Error creating backup: #{e.message}"
       redirect_to backup_path
     end
