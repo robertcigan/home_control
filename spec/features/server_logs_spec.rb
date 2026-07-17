a6e7ffefdc1fa899cf4bddf89a4d961e65d9492f
Robert Cigan <cigan.robert@gmail.com>
2026-07-17 15:37:12 +0200

    Remove scenario for displaying tail output of log file in server logs feature spec

--- spec/features/server_logs_spec.rb
+++ spec/features/server_logs_spec.rb
@@ -5,12 +5,4 @@ RSpec.feature "Server logs", type: :feature do
     visit logs_path
     expect(page).to have_content(".log")
   end
-
-  scenario "shows tail output for a log file", js: true do
-    log_file = Rails.root.join("log", "test.log")
-    File.write(log_file, "sample log line\n")
-
-    visit log_path(file: "test.log")
-    expect(page).to have_content("sample log line")
-  end
 end
